6417ff56211d9a4688d1473a08a12c340e50e46c
Nia Mutiara <nmutiara@gmail.com>
2013-03-06 16:55:20 +0800

    Initial Commit

---
+++ AnimatedPopup/PPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PPAppDelegate.h
+//  AnimatedPopup
+//
+//  Created by Nia Mutiara on 6/3/13.
+//  Copyright (c) 2013 Nia Mutiara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnimatedPopup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AnimatedPopup
+//
+//  Created by Nia Mutiara on 6/3/13.
+//  Copyright (c) 2013 Nia Mutiara. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PPAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PPAppDelegate class]));
+    }
+}
